525c514318fffe9b4559bb36a4d966b32286fc8a
Fatih <kayretlifatih@gmail.com>
2022-10-25 16:23:58 +0300

    dosya konumu değişti

--- userdata.sh
+++ userdata.sh
@@ -4,12 +4,12 @@ apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
 TOKEN="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"
-git clone https://$TOKEN@<YOUR GITHUB REPO URL>
-cd /home/ubuntu/<YOUR GITHUB REPO NAME>
+git clone https://$TOKEN@<https://github.com/kayretlifatih/aws-capstone.git>
+cd /home/ubuntu/Project-503-Capstone-Project-Blog-Page-App-(Django)-on-AWS-Environment
 apt install python3-pip -y
 apt-get install python3.7-dev libmysqlclient-dev -y
 pip3 install -r requirements.txt
-cd /home/ubuntu/<YOUR GITHUB REPO NAME>/src
+cd /home/ubuntu/Project-503-Capstone-Project-Blog-Page-App-(Django)-on-AWS-Environment/src
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
